18a440b21ec46130316ab1e4b4c1297b687b4445
teejzee <thijs.zijp@gmail.com>
2026-09-16 17:44:56 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// API Configuration - Generated at build time
-window.API_CONFIG = {
-  BASE_URL: 'http://localhost:3000',
-  AVAILABILITY_ENDPOINT: '/api/availability',
-  SCRAPE_NOW_ENDPOINT: '/api/scrape-now'
-};
