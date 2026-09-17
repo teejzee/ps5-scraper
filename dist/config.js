f93aace0a1e37303ce83f9f4af47e2c656eaa8d5
teejzee <thijs.zijp@gmail.com>
2026-09-17 23:58:14 +0200

    Delete dist directory

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-// API Configuration - Development configuration
-window.API_CONFIG = {
-  BASE_URL: 'http://localhost:3000',
-  AVAILABILITY_ENDPOINT: '/api/availability',
-  SCRAPE_NOW_ENDPOINT: '/api/scrape-now'
-};
